b2e41331296433ea2156599d1337989098a1f474
jmercer <jessy.mercer@gmail.com>
2020-05-19 18:39:26 +0200

    Tried fix bug

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('',include("blog.urls")),
 ]
