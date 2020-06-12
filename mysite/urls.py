a630f0db85c442c4cd052294c36f8198fb72e7c2
jmercer <jessy.mercer@gmail.com>
2020-06-12 15:37:09 +0200

    Changed path mysites url for loing

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('accounts/login/',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('',include("blog.urls")),
 ]
